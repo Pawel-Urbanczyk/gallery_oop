e1a337c4e519f5d51c7bbb2ee1e675df08e97d06
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-05-14 13:13:45 +0200

    Dodanie pliku database.php z klasa Database, otwierajaca polaczenie z baza danych.

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -5,10 +5,4 @@ define('DB_USER','pawel');
 define('DB_PASS','123');
 define('DB_NAME','gallery_db');
 
-$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS, DB_NAME);
-
-if($connection){
-     echo "true";
-}
-
 ?>
