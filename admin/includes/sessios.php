62380f96ef926756ed96b241268dd3e7a84e03b7
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-05-28 20:12:23 +0200

    Dodanie klasy session.

---
+++ admin/includes/sessios.php
@@ -0,0 +1,19 @@
+<?php
+
+    class Session{
+
+        private $signed_in;
+        public $user_id;
+
+
+        function __construct(){
+
+            session_start();
+
+        }
+
+    }
+
+    $session = new Session();
+
+?>
