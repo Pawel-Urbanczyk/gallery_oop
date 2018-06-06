bbf652c8e8cb5cc1674ba51acf0f3fca8bbdd726
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-06-06 17:44:35 +0200

    Dodanie metody set_file i save w klasie Photo

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -13,8 +13,8 @@ class Photo extends Db_object {
 
     public $tmp_path;
     public $upload_directory = "images";
-    public $custom_errors = array();
-    public $upload_errors = array(
+    public $errors = array();
+    public $upload_errors_array = array(
 
         UPLOAD_ERR_OK => "There is no error",
         UPLOAD_ERR_INI_SIZE => "The uploaded file exceed the upload_max_filesize directive",
@@ -25,6 +25,79 @@ class Photo extends Db_object {
         UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
         UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
 );
+
+
+public function set_file($file){
+
+    if(empty($file) || !$file || !is_array($file)){
+
+        $this->errors[] = "There was no file uploaded here";
+        return false;
+
+    }elseif ($file['error'] !=0){
+
+        $this->errors[] = $this->upload_errors_array[$file['error']];
+        return false;
+
+    }else{
+
+        $this->filename = basename($file['name']);
+        $this->tmp_path = $file['tmp_name'];
+        $this->type = $file['type'];
+        $this->size = $file['size'];
+
+        }
+
+    }
+
+    public function save(){
+
+        if($this->photo_id){
+
+            $this->update();
+
+        }else{
+
+            if(!empty($this->errors)){
+
+                return false;
+
+            }
+
+            if(empty($this->filename) || empty($this->tmp_path)){
+
+                $this->errors[] = "The file was not available";
+                return false;
+
+            }
+
+            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+
+            if(file_exists($target_path)){
+                $this->errors[] = "The file {$this->filename} already exists";
+                return false;
+
+            }
+
+            if(move_uploaded_file($this->tmp_path, $target_path)){
+
+                if($this->create()){
+
+                    unset($this->tmp_path);
+                    return true;
+
+                }
+
+            }else{
+
+                $this->errors[] = "The folder directory probably does not permission";
+                return false;
+
+            }
+
+        }
+
+    }
 
 }
 
