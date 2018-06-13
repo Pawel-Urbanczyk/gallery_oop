7a96940c85b5c1db860791716cb5e97ad3d51ab5
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-06-13 13:54:07 +0200

    Dodanie google chart api

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -33,6 +33,10 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <!--Google chart API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
 </head>
 
 <body>
